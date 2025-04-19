7908c422e6a07beb27037a23865352eb4a3c7045
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-19 23:37:14 +0300

    Adding Request Logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,10 +1,28 @@
+import axios from 'axios';
 import { ActionType } from '../action-types';
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: any) => {
     dispatch({ type: ActionType.SEARCH_REPOSITORIES });
 
     try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (err) {
       if (err instanceof Error) {
         dispatch({
